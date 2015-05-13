c5806bc06c6bf6e4df3c0490788094f3b20eaaf0
John Shen <john_shen@brown.edu>
2015-05-13 18:48:23 -0400

    updating run-server.sh for nginx to forward to the correct port, esp. if it changes according to the env file passed in

--- run-server.sh
+++ run-server.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+# Note: this function is meant to run WITHIN the docker container
 
 function error {
   echo $1 && exit 0 
 }
 
-nc -zw2 mongo 27017 || error "Mongo database not available, exiting..."
-nc -zw2 statserver $ENRICHMENT_PORT || error "Statistics server not available, exiting..."
+ nc -zw2 mongo 27017 || error "Mongo database not available, exiting..."
+ nc -zw2 statserver $ENRICHMENT_PORT || error "Statistics server not available, exiting..."
+ 
+# prepare /etc/nginx/conf.d/magi-site.conf
+TRIM_NAME=${SITE_URL%:*} # trim port name
+TRIM_NAME=${TRIM_NAME#*://} # trim protocol name
+
+# bash hackery
+sed -i -e "s%\$SERVER_NAME_TO_REPLACE%${TRIM_NAME}%;s/\$PORT_TO_REPLACE/${PORT}/" /etc/nginx/conf.d/magi-site.conf
 
 sudo /etc/init.d/nginx start 
 echo "Starting server with forever" 
